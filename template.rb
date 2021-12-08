68efe83670f7ab77fe046d40bf4d576e485c9f45
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-12-08 15:16:18 -0500

    Add project README & dotenv

--- template.rb
+++ template.rb
@@ -1,5 +1,20 @@
+def source_paths
+  ["#{__dir__}/templates"]
+end
+
+template "README.md", force: true
+
 gem_group :development, :test do
-  gem "rspec-rails"
+  gem "rspec-rails", "~> 5.0"
+  gem "dotenv-rails", "~> 2.7"
+end
+
+unless options[:skip_git]
+  append_to_file ".gitignore", <<~EOM
+
+    # Ignore local configuration overrides
+    .env*.local
+  EOM
 end
 
 after_bundle do
