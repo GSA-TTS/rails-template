4a6f6ee11bd57bf46de3d6a234ea2f9fe0aa0c8b
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-06-05 14:53:52 -0400

    Update terraform for 1.0 module versions
    Deal with sensitive output values in bootstrap module

--- lib/generators/rails_template18f/terraform/templates/terraform/bootstrap/providers.tf
+++ lib/generators/rails_template18f/terraform/templates/terraform/bootstrap/providers.tf
@@ -3,13 +3,13 @@ terraform {
   required_providers {
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.15.0"
+      version = "0.53.1"
     }
   }
 }
 
 provider "cloudfoundry" {
-  api_url      = local.cf_api_url
+  api_url      = "https://api.fr.cloud.gov"
   user         = var.cf_user
   password     = var.cf_password
   app_logs_max = 30
