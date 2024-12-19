b075723396ed7338e243c70f47adabf429e9a226
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-12-19 11:44:04 -0500

    Move app terraform into a single module with per-env variables files

---
+++ lib/generators/rails_template18f/terraform/templates/terraform/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.7"
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry/cloudfoundry"
+      version = "1.1.0"
+    }
+    cloudfoundry-community = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.53.1"
+    }
+  }
+
+  backend "s3" {
+    encrypt = "true"
+  }
+}
+
+provider "cloudfoundry" {
+  api_url  = "https://api.fr.cloud.gov"
+  user     = var.cf_user
+  password = var.cf_password
+}
+
+provider "cloudfoundry-community" {
+  api_url  = "https://api.fr.cloud.gov"
+  user     = var.cf_user
+  password = var.cf_password
+}
