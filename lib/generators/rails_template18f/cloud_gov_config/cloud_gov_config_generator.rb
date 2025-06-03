b52b382bc2fab0a9d1944c78f155dade73e0b1aa
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-06-03 16:11:13 -0400

    Fix invocations of CloudGovConfig

--- lib/generators/rails_template18f/cloud_gov_config/cloud_gov_config_generator.rb
+++ lib/generators/rails_template18f/cloud_gov_config/cloud_gov_config_generator.rb
@@ -12,14 +12,6 @@ module RailsTemplate18f
           Install a helper class to retrieve configuration from ENV["VCAP_SERVICES"]
       DESC
 
-      def install_climate_control
-        return if gem_installed?("climate_control")
-        gem_group :test do
-          gem "climate_control", "~> 1.2"
-        end
-        bundle_install
-      end
-
       def install_model_and_test
         copy_file "app/models/cloud_gov_config.rb"
         copy_file "spec/models/cloud_gov_config_spec.rb"
